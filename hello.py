54794bde060d5de57fff6bc6d1e7f85901b3bff6
Makanga Ivan <>
2021-05-17 21:43:46 +0000

    use of the input method for the clients to type in their info

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+#print("Hello World!")
+first_name = input("Please enter your name here: \n")
+second_name = input("Please enter your name here: \n")
+
+_name = first_name.title() + " "  + second_name.title()
+
+age = int(input("Please enter your age:\n"))
+print("Welcome: " + _name + " your age is : " + str(age)  )
